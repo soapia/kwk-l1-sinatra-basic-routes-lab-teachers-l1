96089eba1c5af4397e1c867d40002ecc6231430d
sofia ongele <sofiaongele@gmail.com>
2018-06-12 02:06:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,5 +5,13 @@ class App < Sinatra::Base
   get '/' do 
     "Hello, World!"
   end
-  
+  get '/name' do 
+    "My name is Sofia"
+  end
+  get '/hometown' do 
+    "My hometown is Santa Clarita"
+  end
+  get '/favorite-song' do 
+    "Hello, World!"
+  end
 end
