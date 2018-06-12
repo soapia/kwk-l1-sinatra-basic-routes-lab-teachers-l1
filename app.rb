c964f25f1bfae4c744d7d5d7c32455f64ed30ad1
sofia ongele <sofiaongele@gmail.com>
2018-06-12 02:08:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello, World!"
+  end
+  get '/name' do # at name do this,, and so on
+    "My name is Sofia"
+  end
+  get '/hometown' do 
+    "My hometown is Santa Clarita"
+  end
+  get '/favorite-song' do 
+    "My favorite song is Bonfire"
+  end
 end
